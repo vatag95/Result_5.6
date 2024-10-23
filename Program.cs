dca0ef96af4371489ab883c6e0179637992d3ad7
Denis <denis.vatagin95@gmail.com>
2024-10-23 11:31:46 +0300

    add last method

--- Program.cs
+++ Program.cs
@@ -4,22 +4,30 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Ответьте на вопросы ниже, для заполнения анкеты:");
         AddUser();
     }
 
 
-    static (string name, int Age, string lastName, string petOwner, int petSum, string[] NicknamesPet) AddUser()
+    static (string name, int Age, string lastName, string petOwner, int petSum, string[] NicknamesPet, int colorSum, string[] favorColor) AddUser()
     {
-        (string name, string lastName, int Age, string petOwner, int petSum, string[] NicknamesPet) anketa;
+        (string name, string lastName, int Age, string petOwner, int petSum, string[] NicknamesPet, int colorSum, string[] favorColor) anketa;
 
-        Console.WriteLine("Введите Ваше имя: ");
-        anketa.name = Console.ReadLine();
+        do
+        {
+            Console.WriteLine("Введите Ваше имя: ");
+            anketa.name = Console.ReadLine();
+        } while (CheckStr(anketa.name, out int corrstr));
 
-        Console.WriteLine("Введите Вашу фамилию: ");
-        anketa.lastName = Console.ReadLine();
+        do
+        {
+            Console.WriteLine("Введите Вашу фамилию: ");
+            anketa.lastName = Console.ReadLine();
+        } while (CheckStr(anketa.lastName, out int corrstr));
 
         string age;
         int intage;
+
         do
         {
             Console.WriteLine("Введите Ваш возраст ");
@@ -27,11 +35,14 @@ class MainClass
         }
         while (CheckNum(age, out intage));
 
-        Console.WriteLine("Есть ли у Вас питомец? да/нет");
-        anketa.petOwner = Console.ReadLine();
+        do
+        {
+            Console.WriteLine("Есть ли у Вас питомец? да/нет");
+            anketa.petOwner = Console.ReadLine();
+        } while (CheckStr(anketa.petOwner, out int corrstr));
         if (anketa.petOwner == "да")
         {
-           
+            
             do
             {
                 Console.WriteLine("Сколько у Вас питомцев?");
@@ -41,6 +52,15 @@ class MainClass
 
             anketa.NicknamesPet = petsNickname(anketa.petSum);
         }
+
+        do
+        {
+            Console.WriteLine("Сколько у Вас любимых цветов?");
+            anketa.colorSum = Convert.ToInt32(Console.ReadLine());
+        }
+        while (CheckNum(age, out intage));
+
+        anketa.favorColor = favColor(anketa.colorSum);
 
         Console.ReadKey();
 
@@ -59,9 +79,29 @@ class MainClass
             }
         }
         {
+
+            Console.WriteLine("Ошибка. Введите данные повторно");
             corrnumber = 0;
             return true;
         }
+    }
+    static bool CheckStr(string str, out int corrstr)
+    {
+        if (int.TryParse(str, out int strstr))
+        {
+            Console.WriteLine("Ошибка. Введите данные повторно");
+            corrstr = strstr;
+            return true;
+        }
+        else if (string.IsNullOrEmpty(str) || str.Length > 20)
+        {
+            Console.WriteLine("Ошибка. Введите данные повторно");
+            corrstr = strstr;
+            return true;
+        }
+        
+        corrstr = 0;
+        return false;
     }
 
     static string[] petsNickname(int petSum)
@@ -72,8 +112,11 @@ class MainClass
 
         for (int i = 0; i < petSum; i++)
         {
+            do
+            {
                 Console.WriteLine("Имя Вашего питомца #{0}: ", i + 1);
                 nick = Console.ReadLine();
+            } while (CheckStr(nick, out int corrstr));
             petNicks[i] = nick;
         }
 
@@ -82,7 +125,18 @@ class MainClass
 
     static string[] favColor (int colorCount)
     {
-
+        string[] favColor = new string[colorCount];
+        string color;
+        for (int i = 0; i < colorCount; i++)
+        {
+            do
+            {
+                Console.WriteLine("Ваш любимый цвет #{0}", i + 1);
+                color = Console.ReadLine();
+                
+            } while (CheckStr(color, out colorCount));
+            favColor[i] = color;
+        }
         return favColor;
     }
 }
